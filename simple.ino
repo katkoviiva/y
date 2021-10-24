14dd238c5d9a9a251ce3aeb2def136ad230d5d0d
katkoviiva <47023770+katkoviiva@users.noreply.github.com>
2021-10-24 21:59:21 +0300

    Add files via upload

---
+++ simple.ino
@@ -0,0 +1,98 @@
+// NeoPixel Ring simple sketch (c) 2013 Shae Erisson
+// Released under the GPLv3 license to match the rest of the
+// Adafruit NeoPixel library
+
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+ #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#endif
+
+// Which pin on the Arduino is connected to the NeoPixels?
+#define PIN        11 // On Trinket or Gemma, suggest changing this to 1
+
+// How many NeoPixels are attached to the Arduino?
+#define NUMPIXELS 300 // Popular NeoPixel ring size
+
+// When setting up the NeoPixel library, we tell it how many pixels,
+// and which pin to use to send signals. Note that for older NeoPixel
+// strips you might need to change the third parameter -- see the
+// strandtest example for more information on possible values.
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+#define DELAYVAL 500 // Time (in milliseconds) to pause between pixels
+
+void setup() {
+  // These lines are specifically to support the Adafruit Trinket 5V 16 MHz.
+  // Any other board, you can remove this part (but no harm leaving it):
+#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
+  clock_prescale_set(clock_div_1);
+#endif
+  // END of Trinket-specific code.
+
+  pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
+}
+
+void loop() {
+  pixels.clear(); // Set all pixel colors to 'off'
+
+  // The first NeoPixel in a strand is #0, second is 1, all the way up
+  pixels.setPixelColor(0, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(1, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(2, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(3, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(4, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(5, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(6, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(7, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(8, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(9, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(10, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(11, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(12, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(13, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(14, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(15, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(16, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(17, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(18, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(19, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(20, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(21, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(22, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(23, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(24, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(25, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(26, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(27, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(28, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(29, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(30, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(31, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(32, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(33, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(34, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(35, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(36, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(37, pixels.Color(14, 15, 0));
+  /*pixels.setPixelColor(38, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(1, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(2, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(3, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(4, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(5, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(6, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(7, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(8, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(9, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(10, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(11, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(12, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(13, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(14, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(15, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(16, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(17, pixels.Color(14, 15, 0));
+  pixels.setPixelColor(18, pixels.Color(14, 15, 0));*/
+    pixels.show();   // Send the updated pixel colors to the hardware.to the count of pixels minus one.
+  
+}
